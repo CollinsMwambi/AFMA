86a8addc8ea1e618e87098dfb0739fcd73c9f767
Collins Mwambi <77346740+CollinsMwambi@users.noreply.github.com>
2021-08-06 02:59:24 +0300

    commits

--- adminpage.php
+++ adminpage.php
@@ -1,82 +1,72 @@
 <?php
 
 session_start();
- if(!isset($_SESSION["loggedin"]) ){
-    header("location:login.php");
-}
 ?>
-
 <html>
-    <head>
-        <link rel="stylesheet" href="admin.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-   <title>Admin</title>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="ac.css">
+<title>Admin</title>
+</head>
+<body>
 
-    </head>
-    <body>
-        <div class="d-flex" id="wrapper">
-<div class="bg-white">
-    <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold-text-uppercase">
+<div class="container-fluid">
+    <div class="row min-vh-100 flex-column flex-md-row">
+      <aside class="col-12 col-md-3 col-xl-2 p-0 bg-dark flex-shrink">
+        <nav class="navbar navbar-expand-md navbar-dark bd-dark flex-md-column flex-row align-items-center py-2 text-center sticky-top " id="sidebar">
+          <div class="text-center p-3">
+            
+           <a class="navbar-brand mx-0 font-weight-bold  text-nowrap"> <i class="fas fa-user-shield"></i> <?php echo($_SESSION["username"]); ?></a>
+          </div>
+              <button type="button" class="navbar-toggler border-0 order-1" data-toggle="collapse" data-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+          <div class="collapse navbar-collapse order-last" id="nav">
+            <ul class="navbar-nav flex-column w-100 justify-content-center">
 
-<i class=""></i>AFMA Admin
+            <li class="nav-item">
+              <a  class="nav-link active">Welcome <?php echo($_SESSION["username"]); ?> you're Logged in as Admin </a>
+            </li>
+            
+            <li class="nav-item">
+              <a href="#" class="nav-link "><i class="far fa-address-card"></i> My profile</a>
+            </li>
+            
+            <li class="nav-item">
+              <a href="users.php" class="nav-link "><i class="far fa-user"></i> Users</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link"><i class="fas fa-prescription-bottle"></i> Products</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link"><i class="fas fa-shopping-basket"></i></i> Orders </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link"><i class="fas fa-user-cog"></i> Admins </a>
+            </li>
+
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link"><i class="fas fa-sign-out-alt"></i> Logout </a>
+            </li>
+
+            
+          </ul>
+          </div>      
+        </nav>   
+      </aside>
+      <main class="col px-0 flex-grow-1">
+        <div class= "container py-3">
+
 </div>
-<div class="list-group list-group-flush my-3">
-    <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active"></a>
-<i class=""></i>Dashboard
-
-</a>
-<a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"></a>
-<i class=""></i>Users
-
-</a>
-<a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"></a>
-<i class=""></i>Admins
-
-</a>
-<a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"></a>
-<i class=""></i>Products
-
-</a>
-<a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"></a>
-<i class=""></i>Logout
-
-</a>
-
-</div>
-
-</div>
-<div id="page-content-wrapper">
-            <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
-                <div class="d-flex align-items-center">
-                    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-                    <h2 class="fs-2 m-0">Dashboard</h2>
-                </div>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
-                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-user me-2"></i><?php echo($_SESSION["username"]); ?>
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Profile</a></li>
-                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                <li><a class="dropdown-item" href="#">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+</main>
+    </div>
+  </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
-    </html>
+
+
+</html>
