4e090fb3c5f57d7940664054525010c234ccdd17
Collins Mwambi <77346740+CollinsMwambi@users.noreply.github.com>
2021-08-30 16:58:05 +0300

    my commit

---
+++ welcome.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.87.0">
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Welcome to Afma</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" >AFMA</a>
+      
+      
+        <form class="d-flex">
+        <a class="nav-link" href="#">Already a member? Login</a>
+         
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<main>
+
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+      <img src="final.jpg" width="10%" height="10%"  aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/>
+
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1 >Welcome to AFMA</h1>
+            <p>Your no.1 Online drugstore.</p>
+            <p><a class="btn btn-lg btn-primary" href="registration.php">Sign up today</a></p>
+          </div>
+        </div>
+      </div>
+     
+      <div class="carousel-item">
+      <img src="wu.jpg" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/>
+
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Why us?</h1>
+            <p>Save time by avoiding queues and pharmacy visits or experiencing stock-outs. You have the products you need at your fingertips</p>
+            
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <img src="l.jpg" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/>
+
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <h1>Guranteed Quality</h1>
+            <p>All our medicines, health products are genuine and are high quality.</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+  <!-- Marketing messaging and featurettes
+  ================================================== -->
+  <!-- Wrap the rest of the page in another container to center all the content. -->
+
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class="col-lg-4">
+      <img src="blog.jpg" width="140" height="140" class="rounded-circle " role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text>
+
+        <h2>Our blog</h2>
+       <p>Read our Blog to get to know more about our products and get tips from doctors on matters healthy living</p>
+        <p><a class="btn btn-dark" href="#">Blog &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+      <img src="news.jpg" width="140" height="140" class="rounded-circle " role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text>
+
+        <h2>newsletter</h2>
+        <p>Subscribe to our newsletter to see announcements about our products and latest discounts</p>
+        <p><a class="btn btn-dark" href="#">Newsletter &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+      <img src="cpj.jpg" width="140" height="140" class="rounded-circle " role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text>
+
+        <h2>About us</h2>
+        <p>Find out who we are and what we aim to achieve</p>
+        <p><a class="btn btn-dark" href="#">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+
+
+    <!-- START THE FEATURETTES -->
+
+    <!-- <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+        <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider"> -->
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+  <footer class="container">
+
+  <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
+ 
+  <p>
+  <i class="fas-solid fa-envelope"></i>afma@gmail.com
+    
+  </p>
+  <p>
+    <i class="fas fa-phone"></i>   0700000000
+  </p>
+ 
+    <p>&copy; 2021 AFMA, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+      
+  </body>
+</html>
--- welcomef.php
+++ welcomef.php
@@ -102,6 +102,119 @@ aria-labelledby="navbarDropdown">
 </div>
     </div>
   </div>
+
+  <?php
+session_start();
+include("db-con.php");
+  
+if (isset($_POST['btns']))
+{
+  $productname=$_POST['productname'];
+  $q="select * from products where productname='$productname'";
+  $query=mysqli_query($conn,$q);
+} 
+else 
+{
+  $q= "select * from products";
+  $query=mysqli_query($conn,$q);
+}  
+       
+        
+?>
+
+
+    <link rel="stylesheet" href=
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="showpu.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        
+        <!-- top -->
+        <div class="row">
+            <div class="col-lg-8">
+                <h1>Products</h1>
+                <!-- <a  href="addproducts.php" class="btn btn-outline-success">Add Products</a>
+                <a  href="adminpage.php" class= "btn btn-dark">Go back</a> -->
+            </div>
+           
+            <div class="col-lg-4"> 
+                <div class="row">
+                    <div class="col-lg-8">
+                    
+                        <!--  Filtering-->
+                        <!-- <form method="post" action="">
+                            <input type="text" 
+                                class="form-control" 
+                                name="productname">
+                          
+                            <div class="col-lg-4" 
+                                method="post">
+                                <input type="submit" 
+                                class="btn btn-danger float-right" 
+                                name="btns" value="Search">
+                            </div>
+                        </form> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- product Cards -->
+        <div class="row mt-4">
+            <?php
+            $result = mysqli_query($conn, "SELECT * FROM products");
+                while ($qq=$result->fetch_assoc())
+                {
+            ?>
+
+            <div class="col-lg-4">
+                <div class="card">
+                <img src="imageuploads/<?=$qq['filename']?>" width="350" height="250">
+                    <div class="card-body">
+                   
+                        <h5 class="card-title">
+                        <?php echo $qq['productname']; ?>
+                        </h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Price(ksh)=
+                            <?php echo
+                            $qq['price']; ?>
+                        </h6>
+                        <h6 class="card-subtitle mb-2 text-muted">Quantity  =
+                            <?php echo
+                            $qq['description']; ?>
+                        </h6>
+
+                    <!-- <h8>    
+                        <a class="btn btn-danger" href=
+                        "productdelete.php?id=<?php echo $qq['id']; ?>" onclick="return confirm('Are you sure?')"
+                            class="card-link">
+                            Delete
+                        </a>
+                        <a  class="btn btn-success" href=
+                        "productupdate.php?id=<?php echo $qq['id']; ?>" 
+                            class="card-link" >
+                            Update
+                        </a>
+                </h8> -->
+                    </div>
+                </div><br>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+
+
+</body>
+
+</html> 
+
+
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
  
 
