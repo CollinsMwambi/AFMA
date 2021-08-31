bbda4489ec622b820d61756e3aee1b3621a75921
Collins Mwambi <77346740+CollinsMwambi@users.noreply.github.com>
2021-08-31 06:02:19 +0300

    my comit

--- cart.php
+++ cart.php
@@ -44,6 +44,11 @@ session_start();
 
 
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
  <style>
         @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
 
@@ -75,9 +80,34 @@ session_start();
             background-color: #efefef;
         }
     </style>
-
+    <link href="form-validation.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 <div style="clear: both"></div>
-        <h3 class="title2">Shopping Cart Details</h3>
+<title>Cart</title>
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      
+      <h2>AFMA Checkout</h2>
+      
+      
+    </div>
+  
+
+    <div class="row g-5">
+      <div class="col-md-5 col-lg-4 order-md-last">
+        <h4 class="d-flex justify-content-between align-items-center mb-3">
+          <span class="text-primary">Your cart</span>
+          
+        </h4>
+<!-- 
+    <ul class="list-group mb-3">
+    <li class="list-group-item d-flex justify-content-between lh-sm"> -->
+        <div>
+    <!-- <h6 class="my-0"></h6> -->
         <div class="table-responsive">
             <table class="table table-bordered">
             <tr>
@@ -100,7 +130,7 @@ session_start();
                             <td>
                             ksh <?php echo number_format($value["quantity"] * $value["price"], 2); ?></td>
                             <td><a href="cart.php?action=delete&id=<?php echo $value["id"]; ?>"><span
-                                        class="text-danger">Remove Item</span></a></td>
+                                        class="btn btn-danger">Remove Item</span></a></td>
 
                         </tr>
                         <?php
@@ -116,62 +146,105 @@ session_start();
                     }
                 ?>
             </table>
+            
         </div>
-
+       
     </div>
-
-
-
-
-
-
-<!-- <?php
-session_start();
-    require_once('db-con.php');
-    if (isset($_POST['ssubmit'])) {
-    }
-
-        ?>
-        <div style="clear: both"></div>
-        <h3 class="title 2">Shopping Cart Details</h3>
-        <div class="table table-bordered">
-
-
-        <tr>
-            <th width="30%">Product Name</th>
-            <th width="10%">Quantity</th>
-            <th width="13%">Price</th>
-            <th width="10%">Total price</th>
-            <th width="17%">Remove item</th>
-        </tr>
-        <?php
-        if(!empty($_SESSION["cart"])){
-$total=0;
-foreach($_SESSION["cart"] as $key=> $value){
- ?>
-<tr>
-   <td><?php echo $value["productname"];?></td> 
-   <td><?php echo $value["quantity"];?></td> 
-   <td><?php echo $value["price"];?></td> 
-   <td><?php echo number_format(number: $value["quantity"] * $value["price"],decimals:2)?></td> 
-   <td><a href="cart.php?action=delete&id=<?php echo $value["id"]?>"><span class="text-danger">Remove Item</span></a></td>
-
-  </tr>
-  <?php
-  $total=$total + ($value["quantity"] * $value["price"]);
-}
-  ?>
-  <tr>
-      <td colspan="3" align="right">Total</td>
-      <th align="right">$ <?php echo number_format($total, decimals:2);?></th>
-  <td></td>
-    </tr>
-    <?php
-}
     
+    </div>
     
-    
-    ?>
+    <div class="col-md-7 col-lg-8">
+        <h4 class="mb-3">Billing address</h4>
+        <form class="needs-validation" novalidate>
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">First name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="<?php echo($_SESSION["FName"]); ?>" required>
+              
+            </div>
 
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Last name</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="<?php echo($_SESSION["LName"]); ?>" required>
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="username" class="form-label">Username</label>
+              <div class="input-group has-validation">
+               
+                <input type="text" class="form-control" id="username" placeholder="Username"value="<?php echo($_SESSION["username"]); ?>" required>
+              
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="email" class="form-label">Email </label>
+              <input type="email" class="form-control" id="email" value="<?php echo($_SESSION["email"]); ?>" required>
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address" class="form-label">Address</label>
+              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
+              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+            </div>
+
+
+          
+
+          <hr class="my-4">
+
+          <h4 class="mb-3">Payment</h4>
+
+          
+
+          <div class="row gy-3">
+            <div class="col-md-6">
+              <label for="cc-name" class="form-label">Phone number</label>
+              <input type="text" class="form-control" id="cc-name" placeholder="" required>
+              <small class="text-muted">Enter phone number</small>
+              <div class="invalid-feedback">
+                Name on card is required
+              </div>
+            </div>
+
+            
+            </div>
+
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-dark btn-lg" type="submit">Checkout</button>
+        </form>
+      </div>
+    </div>
+  </main>
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2021  AFMA</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
 </div>
-</div> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
+
